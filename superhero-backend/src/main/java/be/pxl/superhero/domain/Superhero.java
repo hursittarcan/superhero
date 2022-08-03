7467b928756a3a13b423238268cd35cdcbef8231
HursitTarcan <HursitTarcan@gmail.com>
2022-08-03 13:58:37 +0200

    superhero entity class added

--- superhero-backend/src/main/java/be/pxl/superhero/domain/Superhero.java
+++ superhero-backend/src/main/java/be/pxl/superhero/domain/Superhero.java
@@ -1,4 +1,56 @@
 package be.pxl.superhero.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Superhero {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String superHeroName;
+
+    public Superhero() {
+        //JPA ONLY
+    }
+
+    public Superhero(String firstName, String lastName, String superHeroName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.superHeroName = superHeroName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSuperHeroName() {
+        return superHeroName;
+    }
+
+    public void setSuperHeroName(String superHeroName) {
+        this.superHeroName = superHeroName;
+    }
 }
