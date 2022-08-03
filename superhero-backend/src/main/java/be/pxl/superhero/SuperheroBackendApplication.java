e07042041581e0d87f9b577a5da22e32bde7c43c
HursitTarcan <HursitTarcan@gmail.com>
2022-08-03 13:46:57 +0200

    initial

---
+++ superhero-backend/src/main/java/be/pxl/superhero/SuperheroBackendApplication.java
@@ -0,0 +1,13 @@
+package be.pxl.superhero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroBackendApplication.class, args);
+	}
+
+}
